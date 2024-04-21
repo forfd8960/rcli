66fe60ead6b876ac0db10db350855b39665d8dd8
forfd8960 <forfd8960@gmail.com>
2024-04-21 12:41:14 +0800

    Handle CSV SubCommand in RCli

--- README.md
+++ README.md
@@ -18,3 +18,50 @@ refer: https://github.com/EmbarkStudios/cargo-deny/pull/420
 [advisories]
 git-fetch-with-cli = true
 ```
+
+## CSV
+
+### install dependency
+
+```sh
+cargo add clap --features derive
+
+cargo add csv
+
+cargo add serde --features derive
+
+cargo add anyhow
+
+cargo add serde-json
+```
+
+```sh
+Usage: rcli <COMMAND>
+
+Commands:
+  csv   convert csv to json
+  help  Print this message or the help of the given subcommand(s)
+
+Options:
+  -h, --help     Print help
+  -V, --version  Print version
+
+
+❯ cargo run -- csv -i test.csv
+warning: unused manifest key: package.author
+   Compiling rcli v0.1.0
+    Finished dev [unoptimized + debuginfo] target(s) in 0.38s
+     Running `target/debug/rcli csv -i test.csv`
+Opts { cmd: Csv(CsvOpts { input: "test.csv", output: "output.json", delimiter: ',', header: true }) }
+```
+
+### Value parser
+
+```sh
+❯ cargo run -- csv -i test.csv
+warning: unused manifest key: package.author
+   Compiling rcli v0.1.0
+    Finished dev [unoptimized + debuginfo] target(s) in 0.45s
+     Running `target/debug/rcli csv -i test.csv`
+error: invalid value 'test.csv' for '--input <INPUT>': File is not exists
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,78 @@
-fn main() {
-    println!("Awesome Rust Cli");
+use std::fs;
+
+use anyhow::{self};
+use clap::Parser;
+use csv::Reader;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "PascalCase")]
+struct Player {
+    name: String,
+    position: String,
+    #[serde(rename = "DOB")]
+    dob: String,
+    nationality: String,
+    #[serde(rename = "Kit Number")]
+    kit: u8,
+}
+
+// rcli csv -i input.csv -o output.json --header -d ','
+#[derive(Debug, Parser)]
+#[command(name="rcli", version, author, about, long_about = None)]
+struct Opts {
+    #[command(subcommand)]
+    cmd: SubCommand,
+}
+
+#[derive(Debug, Parser)]
+enum SubCommand {
+    #[command(name = "csv", about = "convert csv to json")]
+    Csv(CsvOpts),
+}
+
+#[derive(Debug, Parser)]
+struct CsvOpts {
+    #[arg(short, long, value_parser = verify_input)]
+    input: String,
+    #[arg(short, long, default_value = "output.json")]
+    output: String,
+    #[arg(short, long, default_value_t = ',')]
+    delimiter: char,
+    #[arg(long, default_value_t = true)]
+    header: bool,
+}
+
+// clap value validation
+fn verify_input(f: &str) -> std::result::Result<String, String> {
+    if std::path::Path::new(f).exists() {
+        Ok(f.into())
+    } else {
+        Err("File is not exists".into())
+    }
+}
+
+fn main() -> anyhow::Result<()> {
+    let opts = Opts::parse();
+    println!("{:?}", opts);
+    handle_opts(opts)
+}
+
+fn handle_opts(opts: Opts) -> anyhow::Result<()> {
+    match opts.cmd {
+        SubCommand::Csv(csv_opts) => {
+            let mut reader = Reader::from_path(csv_opts.input)?;
+            let mut players = Vec::with_capacity(128);
+            for record in reader.deserialize() {
+                let r: Player = record?;
+                players.push(r);
+                // println!("{:?}", r);
+            }
+
+            let player_data = serde_json::to_string_pretty(&players)?;
+            fs::write(csv_opts.output, player_data)?;
+
+            anyhow::Ok(())
+        }
+    }
 }
