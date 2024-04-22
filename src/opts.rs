cf0b46e2c74f2f4a7db57491c63a734239f37c23
forfd8960 <forfd8960@gmail.com>
2024-04-22 09:01:27 +0800

    Add format option to CsvOpts

--- src/opts.rs
+++ src/opts.rs
@@ -1,3 +1,6 @@
+use std::str::FromStr;
+
+use anyhow::anyhow;
 use clap::Parser;
 
 // rcli csv -i input.csv -o output.json --header -d ','
@@ -14,12 +17,21 @@ pub enum SubCommand {
     Csv(CsvOpts),
 }
 
+#[derive(Debug, Clone, Copy)]
+pub enum OutputFormat {
+    Json,
+    Yaml,
+    Toml,
+}
+
 #[derive(Debug, Parser)]
 pub struct CsvOpts {
     #[arg(short, long, value_parser = verify_input)]
     pub input: String,
     #[arg(short, long, default_value = "output.json")]
     pub output: String,
+    #[arg(short, long, value_parser = parse_format, default_value = "json")]
+    pub format: OutputFormat,
     #[arg(short, long, default_value_t = ',')]
     pub delimiter: char,
     #[arg(long, default_value_t = true)]
@@ -27,10 +39,26 @@ pub struct CsvOpts {
 }
 
 // clap value validation
-fn verify_input(f: &str) -> std::result::Result<String, String> {
+fn verify_input(f: &str) -> std::result::Result<String, &'static str> {
     if std::path::Path::new(f).exists() {
         Ok(f.into())
     } else {
-        Err("File is not exists".into())
+        Err("File is not exists")
+    }
+}
+
+fn parse_format(f: &str) -> std::result::Result<OutputFormat, anyhow::Error> {
+    f.parse::<OutputFormat>()
+}
+
+impl FromStr for OutputFormat {
+    type Err = anyhow::Error;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.to_lowercase().as_str() {
+            "json" => Ok(OutputFormat::Json),
+            "toml" => Ok(OutputFormat::Toml),
+            "yaml" => Ok(OutputFormat::Yaml),
+            _ => Err(anyhow!("Not suppoered format")),
+        }
     }
 }
