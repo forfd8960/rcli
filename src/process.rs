cf8c601216d340859f3a862926bf6008e06b193b
forfd8960 <forfd8960@gmail.com>
2024-04-22 08:38:03 +0800

    Refactor process_csv method to make it more general

--- src/process.rs
+++ src/process.rs
@@ -1,31 +1,21 @@
 use std::fs;
 
-use serde::{Deserialize, Serialize};
-
 use csv::Reader;
-
-#[derive(Debug, Deserialize, Serialize)]
-#[serde(rename_all = "PascalCase")]
-pub struct Player {
-    name: String,
-    position: String,
-    #[serde(rename = "DOB")]
-    dob: String,
-    nationality: String,
-    #[serde(rename = "Kit Number")]
-    kit: u8,
-}
+use serde_json::Value;
 
 pub fn process_csv(input: &str, output: &str) -> anyhow::Result<()> {
     let mut reader = Reader::from_path(input)?;
-    let mut players = Vec::with_capacity(128);
-    for record in reader.deserialize() {
-        let r: Player = record?;
-        players.push(r);
-        // println!("{:?}", r);
+    let mut data = Vec::with_capacity(128);
+    let headers = reader.headers()?.clone();
+    println!("{:?}", headers);
+
+    for record in reader.records() {
+        let r = record?;
+        let json_value = headers.iter().zip(r.iter()).collect::<Value>();
+        data.push(json_value);
     }
 
-    let player_data = serde_json::to_string_pretty(&players)?;
+    let player_data = serde_json::to_string_pretty(&data)?;
     fs::write(output, player_data)?;
 
     anyhow::Ok(())
