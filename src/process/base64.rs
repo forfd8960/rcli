87f7c98c14c2ec6a6ec8f4bb58be3ece03591de2
forfd8960 <forfd8960@gmail.com>
2024-04-24 15:18:21 +0800

    feature: refactor get_reader in process::base64

--- src/process/base64.rs
+++ src/process/base64.rs
@@ -5,19 +5,25 @@ use base64::{engine::general_purpose, prelude::Engine};
 use crate::cli::base64::Base64Format;
 
 pub fn encode(input: &str, format: Base64Format) -> anyhow::Result<()> {
-    let buf = read_input(input)?;
+    let mut reader = get_reader(input)?;
+
+    let mut buf = Vec::new();
+    let _ = reader.read_to_end(&mut buf)?;
 
     let encode = match format {
         Base64Format::Standard => general_purpose::STANDARD.encode(buf),
         Base64Format::UrlSafe => general_purpose::URL_SAFE_NO_PAD.encode(buf),
     };
 
-    println!("encode: {}", encode);
+    println!("{}", encode);
     anyhow::Ok(())
 }
 
 pub fn decode(input: &str, format: Base64Format) -> anyhow::Result<()> {
-    let buf = read_input(input)?;
+    let mut reader = get_reader(input)?;
+
+    let mut buf = String::new();
+    let _ = reader.read_to_string(&mut buf)?;
     let buf = buf.trim();
 
     let decode = match format {
@@ -25,22 +31,17 @@ pub fn decode(input: &str, format: Base64Format) -> anyhow::Result<()> {
         Base64Format::UrlSafe => general_purpose::URL_SAFE_NO_PAD.decode(buf)?,
     };
 
-    println!("decode: {:?}", decode);
-
     let decode_data = String::from_utf8(decode)?;
-    println!("decode_data: {}", decode_data);
+    println!("{}", decode_data);
     anyhow::Ok(())
 }
 
-fn read_input(input: &str) -> anyhow::Result<String, anyhow::Error> {
-    let mut reader: Box<dyn Read> = if input == "-" {
+fn get_reader(input: &str) -> anyhow::Result<Box<dyn Read>, anyhow::Error> {
+    let reader: Box<dyn Read> = if input == "-" {
         Box::new(std::io::stdin()) // use box::new convert to trait object
     } else {
         Box::new(std::fs::File::open(input)?)
     };
 
-    let mut buf = String::new();
-    let _ = reader.read_to_string(&mut buf)?;
-
-    anyhow::Ok(buf)
+    anyhow::Ok(reader)
 }
