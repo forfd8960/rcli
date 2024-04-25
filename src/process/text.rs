568d84097c05136cd39c4198d92438e5f76645e0
forfd8960 <forfd8960@gmail.com>
2024-04-25 10:04:59 +0800

    feature: Implement TextSign and TextVerify for ED25119

--- src/process/text.rs
+++ src/process/text.rs
@@ -1,6 +1,7 @@
 use std::{fs, io::Read};
 
 use base64::{engine::general_purpose, Engine};
+use ed25519_dalek::{ed25519::signature::Signer, Signature, SigningKey, VerifyingKey};
 
 use crate::{
     cli::text::{TextSignFormat, TextSignOpts, TextVerifyOpts},
@@ -19,13 +20,22 @@ pub struct Blake3 {
     key: [u8; 32],
 }
 
-// pub struct ED25519Signer {
-//     key: [u8; 32],
-// }
+impl Blake3 {
+    fn try_new(file: &str) -> anyhow::Result<Self> {
+        let key = fs::read(file)?;
+        let key = &key[..32];
+        let key = key.try_into()?;
+        anyhow::Ok(Self { key })
+    }
+}
 
-// pub struct ED25519Verify {
-//     key: [u8; 32],
-// }
+pub struct ED25519Signer {
+    key: SigningKey,
+}
+
+pub struct ED25519Verify {
+    key: VerifyingKey,
+}
 
 pub fn process_sign(sign_opts: TextSignOpts) -> anyhow::Result<()> {
     let mut reader = utils::get_reader(&sign_opts.input)?;
@@ -34,11 +44,7 @@ pub fn process_sign(sign_opts: TextSignOpts) -> anyhow::Result<()> {
 
     let sign = match sign_opts.format {
         TextSignFormat::Blake3 => {
-            let key = fs::read(sign_opts.key)?;
-            let key = &key[..32];
-            let key = key.try_into()?;
-
-            let signer = Blake3 { key };
+            let signer = Blake3::try_new(&sign_opts.key)?;
             signer.sign(&mut reader)?
         }
         TextSignFormat::Ed25519 => {
@@ -72,5 +78,25 @@ impl TextVerify for Blake3 {
         let hash = blake3::hash(&buf);
         let hash = hash.as_bytes();
         anyhow::Ok(hash == sig)
+    }
+}
+
+impl TextSign for ED25519Signer {
+    fn sign(&self, reader: &mut dyn Read) -> anyhow::Result<Vec<u8>> {
+        let mut buf = Vec::new();
+        let _ = reader.read_to_end(&mut buf);
+
+        let sig = self.key.sign(&buf);
+        anyhow::Ok(sig.to_bytes().to_vec())
+    }
+}
+impl TextVerify for ED25519Verify {
+    fn verify(&self, mut reader: impl Read, sig: &[u8]) -> anyhow::Result<bool> {
+        let mut buf = Vec::new();
+        let _ = reader.read_to_end(&mut buf);
+
+        let signature = Signature::from_bytes(sig.try_into()?);
+        let verify_result = self.key.verify_strict(&buf, &signature).is_ok();
+        anyhow::Ok(verify_result)
     }
 }
